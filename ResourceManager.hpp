84cf28d606ca479ecab6cdb96cd07e89cb06fc07
karolleq268 <karolleq268@proton.me>
2024-11-20 21:13:45 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,42 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
+class ResourceManager 
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource; 
+
+public:
+    ResourceManager() : resource(new Resource()) 
+    {
+        std::cout << "ResourceManager created\n";
+    }
+
+    ~ResourceManager() 
+    {
+        delete resource;
+        std::cout << "ResourceManager destroyed\n";
+    }
+
+    ResourceManager(const ResourceManager& other)
+        : resource(new Resource(*other.resource)) 
+    {
+        std::cout << "ResourceManager copied\n";
+    }
+
+    ResourceManager& operator=(const ResourceManager& other) 
+    {
+        if (this != &other) 
+        {
+            delete resource;
+            resource = new Resource(*other.resource);
+            std::cout << "ResourceManager copy-assigned\n";
+        }
+        return *this;
+    }
+
+    double get() 
+    {
+        return (*resource).get();
+    }
 };
